6ec0568baea37b5f69a8b3570359657631c04b42
Tony Issakov <tissakov@thefrontiergroup.com.au>
2009-09-25 21:24:14 +0800

    initial commit

---
+++ clone_and_link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+cd &&
+[ -d '.dot-files' ] || git clone git://github.com/tissak/dot-files.git .dot-files &&
+ls -1d .dot-files/.* | while read f; do
+  [ "$f" == '.dot-files/.' ] ||
+  [ "$f" == '.dot-files/..' ] ||
+  [ "$f" == '.dot-files/.git' ] ||
+  ln -vsf "$f" .
+done
